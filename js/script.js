00ae2296eed7faedad5ec4e35e53da056480d99f
lorenzorizzolo <lolloriz36@gmail.com>
2024-10-29 00:26:08 +0100

    Aggiunto

---
+++ js/script.js
@@ -0,0 +1,36 @@
+$(document).on('scroll', () => {
+    const scrollTop = $(document).scrollTop();
+    const documentHeight = $(document).height();
+    const windowHeight = $(window).height();
+    const scrollPercent = (scrollTop / (documentHeight - windowHeight)) * 100;
+
+    if (scrollTop > 100) {
+        $(".scroll_top").css({ "opacity": "1" });
+        const newWidth = 50 + (scrollPercent * 0.5);    
+        $("header h1 img").css({
+            "width": `${newWidth}px`,
+            "opacity": 1,
+        });
+    } else {
+        $(".scroll_top").css({ "opacity": "0" });
+        $("header h1 img").css({
+            "width": "0px",
+            "opacity": 0, 
+        });
+    }
+});
+
+$('.scroll-btn').on('click', function() {
+    // Ottieni il valore dell'attributo data-target
+    const target = $(this).data('target');
+
+    $('html, body').animate({
+        scrollTop: $(target).offset().top - 200
+    }, 800); // 800 millisecondi per l'animazione, puoi cambiare il tempo se vuoi
+});
+
+$(".scroll_top").on("click", ()=>{
+    $('html, body').animate({
+        scrollTop: 0
+    }, 800); // 800 millisecondi per l'animazione, puoi cambiare il tempo se vuoi
+})
