53053fe3fa3d5cf6523b2931c0ee82addec7289b
lorenzorizzolo <lolloriz36@gmail.com>
2024-10-29 17:53:42 +0100

    Aggiunto

--- js/script.js
+++ js/script.js
@@ -1,26 +1,25 @@
 const isMobile = $(window).width() < 1300;
 
-$(document).ready(()=>{
-    if(!isMobile){
-        $(".panel").removeClass("hide")
-        $(".panel").addClass("show")
+$(document).ready(() => {
+    if (!isMobile) {
+        $(".panel").removeClass("hide").addClass("show");
     }
+
     function updateNavbarHeight() {
         const headerHeight = $("header").height();
         $(".panel .navbar").css("height", headerHeight);
+        $("main").css("padding-top", headerHeight);
     }
     updateNavbarHeight();
     setInterval(updateNavbarHeight, 200);
-})
+});
 
-
-$("header").ready(()=>{
-    $(".panel .navbar").css({"height":$("header").height()})
-})
+$("header").ready(() => {
+    $(".panel .navbar").css({ "height": $("header").height() });
+});
 
 $(document).on('scroll', () => {
     const scrollTop = $(document).scrollTop();
-
     if (scrollTop > 100) {
         $(".scroll_top").css({ "opacity": "1" });
     } else {
@@ -32,31 +31,37 @@ $('.scroll-btn').on('click', function() {
     const target = $(this).data('target');
     $('html, body').animate({
         scrollTop: $(target).offset().top - 200
-    }, 800); // 800 millisecondi per l'animazione, puoi cambiare il tempo se vuoi
+    }, 800);
 });
 
-$(".scroll_top").on("click", ()=>{
+$(".scroll_top").on("click", () => {
     $('html, body').animate({
         scrollTop: 0
-    }, 800); // 800 millisecondi per l'animazione, puoi cambiare il tempo se vuoi
-})
+    }, 800);
+});
 
 function copyDivToClipboard(text, alert_m) {
     navigator.clipboard.writeText(text);
-    alert(alert_m)
+    alert(alert_m);
 }
 
-$(".menu").on("click", (event)=>{
+$(".menu").on("click", (event) => {
     event.stopPropagation();
     $(".panel").removeClass("hide").addClass("show");
-})
-$(".close, .panel .pages button").on("click", ()=>{
-    if(isMobile){
+    $("html").addClass("no-scroll");  // Disabilita lo scroll della pagina
+});
+
+$(".close, .panel .pages button").on("click", () => {
+    if (isMobile) {
         $(".panel").removeClass("show").addClass("hide");
+        $("html").removeClass("no-scroll");  // Riabilita lo scroll della pagina
     }
-})
+});
+
 $(document).on("click", (event) => {
     if (!$(event.target).closest('.panel').length && $(".panel").hasClass("show")) {
         $(".panel").removeClass("show").addClass("hide");
+        $("html").removeClass("no-scroll");  // Riabilita lo scroll quando si chiude la .panel
     }
 });
+
